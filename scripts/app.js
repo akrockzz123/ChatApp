0576f6b81d5ba5430296e0a4ee70343aa6408d76
Aniket Kumar <aniketkumar1601@gmail.com>
2022-06-26 01:23:27 +0530

    added update name functionality

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,10 @@ const chatlist = document.querySelector('.chat-list');
 
 const newchatform = document.querySelector('.new-chat');
 
+const newNameForm = document.querySelector('.update-name');
+
+const updatename =  document.querySelector('.updatedname');
+
 newchatform.addEventListener('submit', e => { // we set up the listener to listen for the changes
 
     e.preventDefault();
@@ -12,6 +16,26 @@ newchatform.addEventListener('submit', e => { // we set up the listener to liste
        .then(() => newchatform.reset())
        .catch(err => console.log(err))
 });
+
+//updating the name
+
+newNameForm.addEventListener('submit', e => {
+
+    e.preventDefault();
+    const newname = newNameForm.newname.value.trim();
+
+    chatroom.updateName(newname);
+
+    newNameForm.reset(); // clearing the name form after submiting
+
+   
+
+    setTimeout(() => 
+        updatename.innerText = `Your name was updated to ${newname}`,3000); // show updated name 
+
+
+});
+
 const chatui = new chatUI(chatlist) // create new chaui class instance by passing the reference of chatlist address
 
 const chatroom = new ChatRoom('gaming', 'shaun'); // created new instance of chats class
